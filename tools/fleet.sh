c9dfd6ae78c2a42985606bf660031b85465620a7
Gautam BT <git-author@l1cache.in>
2020-02-04 14:48:09 +0530

    tools/fleet.sh: Kill all children on exit

--- tools/fleet.sh
+++ tools/fleet.sh
@@ -1,18 +1,19 @@
  #!/bin/bash
 
- set -ex
+set -ex
 
- if [[ $# -ne 1 ]]; then
-     echo "Pass number of devices"
-     echo "Usage: ./createvehicle 10"
-     exit
- fi
+if [[ $# -ne 1 ]]; then
+    echo "Pass number of devices"
+    echo "Usage: ./createvehicle 10"
+    exit
+fi
 
- count=$1
+count=$1
 
- for i in $(seq 1 $count)
- do
-     RUST_LOG=rumqtt,uplink=debug cargo run -- -c config/uplink.toml -i $i -a certs/ &
- done
+for i in $(seq 1 $count)
+do
+    RUST_LOG=rumqtt,uplink=debug cargo run -- -c config/uplink.toml -i $i -a certs/ &
+done
 
- sleep 1000
+trap 'kill $(jobs -p)' EXIT
+wait
