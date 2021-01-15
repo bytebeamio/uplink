1b8e3c6eb35cf4ae0e5f8b5383574a4f74a0bfc5
tekjar <raviteja@bytebeam.io>
2021-01-15 14:55:55 +0530

    Implement write tests in storage

--- disk/src/lib.rs
+++ disk/src/lib.rs
@@ -46,6 +46,14 @@ impl Storage {
         })
     }
 
+    pub fn writer(&mut self) -> &mut BytesMut {
+        &mut self.current_write_file
+    }
+
+    pub fn reader(&mut self) -> &mut BytesMut {
+        &mut self.current_read_file
+    }
+
     fn decrement_reader_index(&mut self) -> io::Result<()> {
         let index = if let Some(index) = self.current_read_file_index { index as isize - 1 } else { return Ok(()) };
 
@@ -121,6 +129,7 @@ impl Storage {
     pub fn flush(&mut self) -> io::Result<()> {
         let mut next_file = self.open_next_write_file()?;
         next_file.write_all(&self.current_write_file[..])?;
+        self.current_write_file.clear();
         Ok(())
     }
 }
@@ -172,4 +181,74 @@ fn parse_index_file(backup_path: &Path) -> io::Result<(File, Option<usize>)> {
     };
 
     Ok((index_file, Some(id)))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use mqttbytes::*;
+    use std::thread;
+    use std::time::Duration;
+    use tempdir::TempDir;
+
+    fn init_backup_folders() -> TempDir {
+        let backup = TempDir::new("/tmp/persist").unwrap();
+
+        if !backup.path().is_dir() {
+            panic!("Folder does not exist");
+        }
+
+        backup
+    }
+
+    #[test]
+    fn flush_creates_new_file_after_size_limit() {
+        // 1036 is the size of a publish message with topic = "hello", qos = 1, payload = 1024 bytes
+        let backup = init_backup_folders();
+        let mut storage = Storage::new(backup.path(), 10 * 1036, 10).unwrap();
+
+        // 2 files on disk and a partially filled in memory buffer
+        for _ in 0..101 {
+            let mut publish = Publish::new("hello", QoS::AtLeastOnce, vec![1; 1024]);
+            publish.pkid = 1;
+            publish.write(storage.writer()).unwrap();
+            storage.flush_on_overflow().unwrap();
+        }
+
+        // 1 message in in memory writer
+        assert_eq!(storage.writer().len(), 1036);
+
+        // other messages on disk
+        let files = get_file_ids(&backup.path()).unwrap();
+        assert_eq!(files, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    fn old_file_is_deleted_after_limit() {
+        let backup = init_backup_folders();
+        let mut storage = Storage::new(backup.path(), 10 * 1036, 10).unwrap();
+
+        // 11 files created. 10 on disk
+        for _ in 0..110 {
+            let mut publish = Publish::new("hello", QoS::AtLeastOnce, vec![1; 1024]);
+            publish.pkid = 1;
+            publish.write(storage.writer()).unwrap();
+            storage.flush_on_overflow().unwrap();
+        }
+
+        let files = get_file_ids(&backup.path()).unwrap();
+        assert_eq!(files, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+        // 11 files created. 10 on disk
+        for _ in 0..10 {
+            let mut publish = Publish::new("hello", QoS::AtLeastOnce, vec![1; 1024]);
+            publish.pkid = 1;
+            publish.write(storage.writer()).unwrap();
+            storage.flush_on_overflow().unwrap();
+        }
+
+        assert_eq!(storage.writer().len(), 0);
+        let files = get_file_ids(&backup.path()).unwrap();
+        assert_eq!(files, vec![2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+    }
 }
