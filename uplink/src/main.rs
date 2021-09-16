8afac72cbbf8cc96a3b7ce35b09b7558beb8582f
Devdutt Shenoi <devdutt@bytebeam.io>
2021-09-16 10:57:51 +0530

    Add default config for OTA

--- uplink/src/main.rs
+++ uplink/src/main.rs
@@ -86,6 +86,10 @@ const DEFAULT_CONFIG: &'static str = r#"
     [streams.action_status]
     topic = "/tenants/{tenant_id}/devices/{device_id}/action/status"
     buf_size = 1
+
+    [ota]
+    enabled = false
+    path = ""
 "#;
 
 /// Reads config file to generate config struct and replaces places holders
