2f7ed1509489545c94e12ff4e321417f9e23e273
Devdutt Shenoi <devdutt@outlook.in>
2022-04-07 13:48:34 +0530

    Ensure lesser frequency of action responses

--- uplink/src/base/actions/ota.rs
+++ uplink/src/base/actions/ota.rs
@@ -81,25 +81,31 @@ impl OtaDownloader {
 
     /// Downloads from server and stores into file
     async fn download(&mut self, resp: Response, mut file: File) -> Result<(), Error> {
+        // NOTE: Commented code for download's content length and completion estimation
         // Supposing content length is defined in bytes
-        let content_length = match resp.content_length() {
-            None => return Err(Error::NoContentLen),
-            Some(0) => return Err(Error::ContentLenZero),
-            Some(l) => l as usize,
-        };
-        let mut downloaded = 0;
+        // let content_length = match resp.content_length() {
+        //     None => return Err(Error::NoContentLen),
+        //     Some(0) => return Err(Error::ContentLenZero),
+        //     Some(l) => l as usize,
+        // };
+        // let mut downloaded = 0;
+        let mut percentage = 0;
         let mut stream = resp.bytes_stream();
 
         // Download and store to disk by streaming as chunks
         while let Some(item) = stream.next().await {
             let chunk = item?;
-            downloaded += chunk.len();
+            // downloaded += chunk.len();
             file.write_all(&chunk)?;
 
-            let percentage = (100 * downloaded / content_length) as u8;
-            let status = ActionResponse::progress(&self.action_id, "Downloading", percentage)
-                .set_sequence(self.sequence());
-            self.send_status(status).await;
+            // NOTE: ensure lesser frequency of action responses
+            if file.metadata()?.len() % 1024 * 1024 == 0 {
+                // let percentage = (100 * downloaded / content_length) as u8;
+                percentage += 1;
+                let status = ActionResponse::progress(&self.action_id, "Downloading", percentage)
+                    .set_sequence(self.sequence());
+                self.send_status(status).await;
+            }
         }
 
         info!("Firmware dowloaded sucessfully");
