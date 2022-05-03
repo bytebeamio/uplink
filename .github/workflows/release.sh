4b32abbb9ebb517a63c1b4b63516cde403fcca63
Devdutt Shenoi <devdutt@outlook.in>
2022-05-03 23:40:44 +0530

    Create build release script

---
+++ .github/workflows/release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+TARGETS="x86_64-unknown-linux-gnu armv7-unknown-linux-gnueabihf"
+BUILD_DIR="build/"
+
+for TARGET in ${TARGETS}
+do
+    echo "Building uplink for ${TARGET}"
+    cross build --release --target ${TARGET}
+done
+
+echo "Creating directory to store built executables: ${BUILD_DIR}"
+mkdir -p ${BUILD_DIR}
+for TARGET in ${TARGETS}
+do
+    echo "uplink binary for ${TARGET} copied into build/ folder"
+    cp target/${TARGET}/release/uplink ${BUILD_DIR}uplink-${TARGET}
+done
