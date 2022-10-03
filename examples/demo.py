a7fee7c92232e27775c5df25a1660b18f88ea37a
gautambt <gautambt@users.noreply.github.com>
2022-10-04 00:43:17 +0530

    feat: Update python example
    The example now shows how to run both data push and respond to actions at the same time.
    Also shows how to execute a simple OTA action.

--- examples/demo.py
+++ examples/demo.py
@@ -1,6 +1,10 @@
 import socket
 import json
 import time
+import os
+import stat
+import shutil
+import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 5555))
@@ -11,7 +15,7 @@ def recv_action(s):
 
 # Constructs a payload and sends it over TCP to uplink
 def send_data(s, payload):
-    send = json.dumps(payload)
+    send = json.dumps(payload) + "\n"
     s.sendall(bytes(send, encoding="utf-8"))
 
 # Constructs a JSON `action_status` as a response to received action on completion
@@ -26,12 +30,41 @@ def action_complete(id):
         "errors": []
     }
 
-while True:
-    action = recv_action(s)
-    print(action)
+def update_firmware(action):
+    payload = json.loads(action['payload'])
+    print(payload)
+    shutil.move(payload["ota_path"], "/tmp/foobar")
+    os.chmod("/tmp/foobar", 0o755)
 
-    resp = action_complete(action["action_id"])
-    print(resp)
-    
+def recv_actions():
+    while True:
+        action = recv_action(s)
+        print(action)
+
+        if action["name"] == "update_firmware":
+            update_firmware(action)
+
+        resp = action_complete(action["action_id"])
+        print(resp)
+        
+        send_data(s, resp)
+
+print("Starting Uplink Bridge App")
+threading.Thread(target=recv_actions).start()
+
+def send_device_shadow(s, sequence):
+    t = int(time.time()*1000)
+    payload = {
+        "stream": "device_shadow",
+        "sequence": sequence,
+        "timestamp": t,
+        "Status": "running" 
+    }
+
+    send_data(s, payload)
+
+sequence = 1
+while True:
     time.sleep(5)
-    send_data(s, resp)
+    send_device_shadow(s, sequence)
+    sequence += 1
