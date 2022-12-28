9bd6be714a1954f4a8ffe1dd80533fd3fc130325
Devdutt Shenoi <devdutt@bytebeam.io>
2022-12-28 10:27:57 +0530

    feat: make `keep_alive` configurable (#80)
    * fix: make keep_alive configurable
    * feat: print `bridge_port` and `keep_alive` on startup, code cleanup

--- uplink/src/base/mod.rs
+++ uplink/src/base/mod.rs
@@ -86,6 +86,7 @@ pub struct Config {
     pub bridge_port: u16,
     pub max_packet_size: usize,
     pub max_inflight: u16,
+    pub keep_alive: u64,
     pub actions: Vec<String>,
     pub persistence: Option<Persistence>,
     pub streams: HashMap<String, StreamConfig>,
