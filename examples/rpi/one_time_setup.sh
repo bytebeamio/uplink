b729817a3caf76c4219d7a65e10cbe10d98e82ea
saikiran-1997 <ysaikiran1997@gmail.com>
2023-01-16 12:22:42 +0530

    Added update_fstab URL to one_time_setup.sh

--- examples/rpi/one_time_setup.sh
+++ examples/rpi/one_time_setup.sh
@@ -1,5 +1,6 @@
 # Run uplink
-# wget update_fstab_url -O /update_fstab.sh
+# get update_fstab.sh
+curl -s -o update_fstab.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/rpi/examples/rpi/update_fstab.sh
 chmod +x update_fstab.sh
 /update_fstab.sh
 mount -a
@@ -11,9 +12,9 @@ mount -a
 #wget systemd_url -O /mnt/download/systemd/systemd.sh
 #wget uplink.service_url -O /mnt/download/systemd/uplink.service
 #wget config.toml -O /mnt/download/config.toml 
-chmod +x /uplink
+#chmod +x /uplink
 
 # uplink.service executes startup.sh, which runs both uplink and bridge app
-ln -s /mnt/download/systemd/uplink.service /etc/systemd/system/multi-user.target.wants/uplink.service
-systemctl daemon-reload
-systemctl start uplink.service
+#ln -s /mnt/download/systemd/uplink.service /etc/systemd/system/multi-user.target.wants/uplink.service
+#systemctl daemon-reload
+#systemctl start uplink.service
