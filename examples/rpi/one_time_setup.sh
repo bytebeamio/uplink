4bea134f8ec95f34963013eaa14e39a699ec85dd
saikiran-1997 <ysaikiran1997@gmail.com>
2023-01-16 12:52:06 +0530

    Modified update_fatsb_next_root URL

--- examples/rpi/one_time_setup.sh
+++ examples/rpi/one_time_setup.sh
@@ -1,17 +1,17 @@
 # Getting one_time_setup.sh
-#curl -s -o one_time_setup.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/rpi/examples/rpi/one_time_setup.sh
+#curl -o one_time_setup.sh -s https://raw.githubusercontent.com/sai-kiran-y/uplink/rpi/examples/rpi/one_time_setup.sh
 
 # Run uplink
 
 # get update_fstab.sh
-curl -s -o update_fstab.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/rpi/examples/rpi/update_fstab.sh
+curl -o update_fstab.sh -s https://raw.githubusercontent.com/sai-kiran-y/uplink/rpi/examples/rpi/update_fstab.sh
 chmod +x update_fstab.sh
 ./update_fstab.sh
 mount -a
 
 # wget update_fstab_next_root_url -O /mnt/download/update_fstab.sh
 # get update_fstab_next_root
-curl -s -o /mnt/download/update_fstab_nextroot.sh https://github.com/sai-kiran-y/uplink/blob/rpi/examples/rpi/update_fstab_next_root.sh 
+curl -o /mnt/download/update_fstab_nextroot.sh -s https://raw.githubusercontent.com/sai-kiran-y/uplink/rpi/examples/rpi/update_fstab_next_root.sh 
 
 #wget uplink_url -O /mnt/download/uplink
 #wget bridge_app_url -O /mnt/download/bridge.py
