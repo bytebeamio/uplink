c9f9ddfc8a127f8d114090445c25b31b57f1fd92
saikiran-1997 <ysaikiran1997@gmail.com>
2023-01-18 15:24:28 +0530

    Added comments

--- examples/rpi/update_fstab_next_root.sh
+++ examples/rpi/update_fstab_next_root.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Get the root partition info
 root_part=`awk -F"root=" '{ print $NF; }' /proc/cmdline | cut -d" " -f1`
+
+# Set download partition to /dev/mmcblk0p4
 download_part=`echo $root_part | sed 's/.$/4/'`
 
+# If root_part is 2, set next_root to 3 and vice versa
 if [ ${root_part: -1} = "2" ]
 then
 	echo "part_two"
@@ -14,6 +19,7 @@ then
 	echo next_root=$next_root
 fi
 
+# Get PARTUUID of the partitions 
 root_uuid=`blkid $root_part | awk -F "PARTUUID=" '{print $NF;}' | cut -d'"' -f2`
 download_uuid=`blkid $download_part | awk -F "PARTUUID=" '{print $NF;}' | cut -d'"' -f2`
 next_root_uuid=`blkid $next_root | awk -F "PARTUUID=" '{print $NF;}' | cut -d'"' -f2`
@@ -21,13 +27,13 @@ echo root_uuid=$root_uuid
 echo next_root_uuid=$next_root_uuid
 echo download_uuid=$download_uuid
 
+# Remove the old entries of fstab, of next_root
 egrep -v "($root_uuid|$download_uuid|$next_root_uuid)" /etc/fstab > /mnt/next_root/etc/_fstab && mv /mnt/next_root/etc/_fstab /mnt/next_root/etc/fstab
 
+# Update the fstab of next_root
 echo "PARTUUID=$next_root_uuid	/	ext4	defaults,noatime	0	1" >> /mnt/next_root/etc/fstab
-
 mkdir -pv /mnt/next_root
 echo "PARTUUID=$root_uuid	/mnt/next_root	ext4	defaults,noatime	0	2" >> /mnt/next_root/etc/fstab
-
 mkdir -pv /mnt/download
 echo "PARTUUID=$download_uuid	/mnt/download	ext4	defaults,noatime	0	2" >> /mnt/next_root/etc/fstab
 
