8b06e97449491ffb827f740852caecdbe3e3201d
saikiran-1997 <ysaikiran1997@gmail.com>
2023-01-18 15:20:05 +0530

    Added comments

--- examples/rpi/update_fstab.sh
+++ examples/rpi/update_fstab.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Get the current root partition
 root_part=`awk -F"root=" '{ print $NF; }' /proc/cmdline | cut -d" " -f1`
+
+# Set download_part to /dev/mmcblk0p4
 download_part=`echo $root_part | sed 's/.$/4/'`
 
+# If root part is p2, next_root is set to 3 and vice versa
 if [ ${root_part: -1} = "2" ]
 then
 	echo "part_two"
@@ -14,6 +19,7 @@ then
 	echo next_root=$next_root
 fi
 
+# Find the UUID of partitions
 root_uuid=`blkid $root_part | awk -F "PARTUUID=" '{print $NF;}' | cut -d'"' -f2`
 download_uuid=`blkid $download_part | awk -F "PARTUUID=" '{print $NF;}' | cut -d'"' -f2`
 next_root_uuid=`blkid $next_root | awk -F "PARTUUID=" '{print $NF;}' | cut -d'"' -f2`
@@ -21,14 +27,15 @@ echo root_uuid=$root_uuid
 echo next_root_uuid=$next_root_uuid
 echo download_uuid=$download_uuid
 
+# Remove the old entries of fstab(if any)
 egrep -v "($root_uuid|$download_uuid|$next_root_uuid)" /etc/fstab > /etc/_fstab && mv /etc/_fstab /etc/fstab
 
+# Add new entries to fstab
 echo "PARTUUID=$root_uuid	/	ext4	defaults,noatime	0	1" >> /etc/fstab
-
 mkdir -pv /mnt/next_root
 echo "PARTUUID=$next_root_uuid	/mnt/next_root	ext4	defaults,noatime	0	2" >> /etc/fstab
-
 mkdir -pv /mnt/download
 echo "PARTUUID=$download_uuid	/mnt/download	ext4	defaults,noatime	0	2" >> /etc/fstab
 
+# Update the mounting
 mount -a
