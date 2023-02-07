ba61d4ef5c3dc51d45c9453486503a717b87fd84
Devdutt Shenoi <devdutt@bytebeam.io>
2023-02-07 23:10:57 +0530

    doc: write example and document config

---
+++ examples/device_shadow.py
@@ -0,0 +1,33 @@
+import socket
+import json
+import time
+import os
+import stat
+import shutil
+import threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("localhost", 4444))
+
+# Constructs a payload and sends it over TCP to uplink
+def send_data(s, payload):
+    send = json.dumps(payload) + "\n"
+    s.sendall(bytes(send, encoding="utf-8"))
+
+
+def send_device_shadow(s, sequence):
+    t = int(time.time()*1000)
+    payload = {
+        "stream": "device_shadow",
+        "sequence": sequence,
+        "timestamp": t,
+        "Status": "running"
+    }
+
+    send_data(s, payload)
+
+sequence = 1
+while True:
+    time.sleep(5)
+    send_device_shadow(s, sequence)
+    sequence += 1
