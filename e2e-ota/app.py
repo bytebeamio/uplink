20220085a5cdace3fd4e9ea34029d339b1d49f36
Devdutt Shenoi <devdutt@bytebeam.io>
2023-02-16 14:30:59 +0530

    feat: test ota on multiple uplink

---
+++ e2e-ota/app.py
@@ -0,0 +1,58 @@
+import socket
+import json
+import time
+import os
+import stat
+import shutil
+import threading
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = "500"+sys.argv[1]
+print("Connecting to :", port)
+s.connect(("localhost", int(port)))
+
+# Converts JSON data received over TCP into a python dictionary
+def recv_action(s):
+    r = s.recv(2048)
+    if not r:
+        return
+    return json.loads(r)
+
+# Constructs a payload and sends it over TCP to uplink
+def send_data(s, payload):
+    send = json.dumps(payload) + "\n"
+    s.sendall(bytes(send, encoding="utf-8"))
+
+# Constructs a JSON `action_status` as a response to received action on completion
+def action_status(id, status, progress):
+    return {
+        "stream": "action_status",
+        "sequence": 0,
+        "timestamp": int(time.time()*1000000),
+        "action_id": id,
+        "state": status,
+        "progress": progress,
+        "errors": []
+    }
+
+
+while True:
+    action = recv_action(s)
+    if not action:
+        continue
+    print(action)
+    action_id = action["action_id"]
+
+    for i in range(100):
+        time.sleep(1)
+        resp = action_status(action_id, "Installing", i)
+        print(resp)
+        send_data(s, resp)
+
+
+    resp = action_status(action_id, "Completed", 100)
+    print(resp)
+    send_data(s, resp)
+
+
---
+++ e2e-ota/test.sh
@@ -0,0 +1,35 @@
+start_devices() {
+    kill_devices;
+
+    for id in $(seq 1 $1)
+    do 
+        # prepare config, please comment below line and download the right config from platform
+        echo "{\"project_id\": \"demo\",\"device_id\": \"$id\",\"broker\": \"stage.bytebeam.io\",\"port\": 1883}" > device_$id.json
+        echo "processes = [] \naction_redirections = { update_firmware = \"install_firmware\" } \n\n[tcpapps.1] \nport = 500$id \nactions= [\"install_firmware\"] \n\n[downloader] \nactions= [\"install_firmware\"] \npath = \"/var/tmp/ota/$id\"" > device_$id.toml
+        start_uplink $id
+
+        sleep 1
+        start_app $id
+    done
+}
+
+start_uplink() {
+    nohup cargo run -- -a device_$1.json -c device_$id.toml -vv > uplink_$1.log 2>&1 &
+    echo $! >> pids
+}
+
+start_app() {
+    nohup python3 app.py $1 > app_$1.log 2>&1 &
+    echo $! >> pids
+}
+
+kill_devices() {
+    while read pid
+    do
+      kill $pid
+    done < pids
+
+    rm pids
+}
+
+$1 $2
