54258929429b9a8b51e62b64580aa47fd3c81c8b
Devdutt Shenoi <devdutt@bytebeam.io>
2023-03-01 19:22:01 +0530

    refactor: remove unnecessary check

--- uplink/src/collector/tcpjson.rs
+++ uplink/src/collector/tcpjson.rs
@@ -45,12 +45,7 @@ pub struct TcpJson {
 
 impl TcpJson {
     pub async fn new(name: String, config: AppConfig, bridge: BridgeTx) -> TcpJson {
-        let actions_rx = if ! config.actions.is_empty() {
-            // TODO: Return Option<Receiver> while registering multiple actions
-            bridge.register_action_routes(&config.actions).await
-        } else {
-            None
-        };
+        let actions_rx = bridge.register_action_routes(&config.actions).await;
 
         // Note: We can register `TcpJson` itself as an app to direct actions to it
         TcpJson { name, config, bridge, actions_rx }
