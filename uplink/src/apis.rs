a39dd19a61476aa3d0d964ae00ff70c9e3ffbea9
Devdutt Shenoi <devdutt@bytebeam.io>
2023-03-29 16:54:21 +0530

    still need to make some fixes to usage of axum

--- uplink/src/apis.rs
+++ uplink/src/apis.rs
@@ -1,30 +1,29 @@
-use axum::{http::Request, routing::post, Router};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Router};
 use log::info;
-use reqwest::StatusCode;
 
 use std::sync::Arc;
 
 use crate::ReloadHandle;
 
-pub fn start(port: u16, handle: ReloadHandle) {
+struct ApiState {
+    handle: ReloadHandle,
+}
+
+pub async fn start(port: u16, handle: ReloadHandle) {
     let address = format!("0.0.0.0:{port}");
     info!("Starting tracing server: {address}");
 
-    let handle = Arc::new(handle);
+    let state = ApiState { handle };
+    let app = Router::new().route("/", post(reload_loglevel)).with_state(Arc::new(state));
 
-    Router::<(), String>::new().route(
-        "/logs",
-        post({
-            let handle = Arc::clone(&handle);
-            move |body| reload_loglevel(handle, body)
-        }),
-    );
+    axum::Server::bind(&address.parse().unwrap()).serve(app.into_make_service()).await.unwrap();
 }
 
-async fn reload_loglevel(handle: Arc<ReloadHandle>, request: Request<String>) -> StatusCode {
-    if handle.reload(request.body()).is_err() {
-        return StatusCode::OK;
+fn reload_loglevel(State(state): State<Arc<ApiState>>, filter: String) -> impl IntoResponse {
+    info!("Reloading tracing filter: {filter}");
+    if state.handle.reload(&filter).is_err() {
+        return StatusCode::INTERNAL_SERVER_ERROR;
     }
 
-    StatusCode::INTERNAL_SERVER_ERROR
+    StatusCode::OK
 }
