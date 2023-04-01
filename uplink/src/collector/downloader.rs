b2ce5a2c7e6db4fa8398dc62fe129a232c7fb061
Devdutt Shenoi <devdutt@bytebeam.io>
2023-04-02 00:07:20 +0530

    fix: create file with provided name only

--- uplink/src/collector/downloader.rs
+++ uplink/src/collector/downloader.rs
@@ -49,7 +49,7 @@ use log::{error, info};
 use reqwest::{Certificate, Client, ClientBuilder, Identity, Response};
 use serde::{Deserialize, Serialize};
 
-use std::fs::{create_dir_all, File, remove_dir_all};
+use std::fs::{create_dir_all, remove_dir_all, File};
 use std::sync::Arc;
 use std::time::Duration;
 use std::{io::Write, path::PathBuf};
@@ -197,13 +197,17 @@ impl FileDownloader {
 
     /// Creates file to download into
     fn create_file(&self, name: &str, file_name: &str) -> Result<(File, String), Error> {
-        // Ensure that directory for downloading file into, of the format `path/to/{version}/`, exists
+        // Ensure that directory for downloading the file into, exists
         let mut download_path = PathBuf::from(self.config.path.clone());
         download_path.push(name);
         let _ = remove_dir_all(&download_path);
         create_dir_all(&download_path)?;
 
         let mut file_path = download_path.to_owned();
+        if file_name.is_empty() {
+            return Err(Error::FileNameMissing(file_name.to_owned()));
+        }
+
         file_path.push(file_name);
         let file_path = file_path.as_path();
         let file = File::create(file_path)?;
@@ -280,7 +284,7 @@ pub struct DownloadFile {
 mod test {
     use std::{collections::HashMap, time::Duration};
 
-    use crate::base::{bridge::Event, DownloaderConfig, MqttConfig, ActionRoute};
+    use crate::base::{bridge::Event, ActionRoute, DownloaderConfig, MqttConfig};
 
     use super::*;
     use flume::TrySendError;
@@ -306,8 +310,10 @@ mod test {
         // Ensure path exists
         std::fs::create_dir_all(DOWNLOAD_DIR).unwrap();
         // Prepare config
-        let downloader_cfg =
-            DownloaderConfig { actions: vec![ActionRoute { name: "firmware_update".to_owned(), timeout: 10 }], path: format!("{DOWNLOAD_DIR}/uplink-test") };
+        let downloader_cfg = DownloaderConfig {
+            actions: vec![ActionRoute { name: "firmware_update".to_owned(), timeout: 10 }],
+            path: format!("{DOWNLOAD_DIR}/uplink-test"),
+        };
         let config = config(downloader_cfg.clone());
         let (events_tx, events_rx) = flume::bounded(2);
         let bridge_tx = BridgeTx { events_tx };
