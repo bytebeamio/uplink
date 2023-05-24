258ee200dbab30e165ad11332124eaed073f038b
root <ysaikiran1997@gmail.com>
2023-05-24 16:12:13 +0530

    fix: Remove run_uplink.sh

--- scripts/run_uplink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start uplink and bridge services
-systemctl enable uplink.service
-#systemctl enable bridge.service
-systemctl enable startup.service
-systemctl start uplink.service
-#systemctl start bridge.service
-systemctl start startup.service
