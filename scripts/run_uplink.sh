5054c1fa0965c093745532f07fd2b56bff686729
root <ysaikiran1997@gmail.com>
2023-05-24 16:01:26 +0530

    fix: Remove bridge app from systemd

--- scripts/run_uplink.sh
+++ scripts/run_uplink.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Start uplink and bridge services
 systemctl enable uplink.service
-systemctl enable bridge.service
+#systemctl enable bridge.service
 systemctl enable startup.service
 systemctl start uplink.service
-systemctl start bridge.service
+#systemctl start bridge.service
 systemctl start startup.service
-
