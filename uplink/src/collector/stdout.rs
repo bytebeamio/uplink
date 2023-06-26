7eca047bd316fc28eb2fdcf890da68b5d5da1e03
Devdutt Shenoi <devdutt@bytebeam.io>
2023-06-26 17:31:14 +0530

    feat: parse stdout loglines, cleanup ansi color

--- uplink/src/collector/stdout.rs
+++ uplink/src/collector/stdout.rs
@@ -1,25 +1,37 @@
+use regex::Regex;
 use tokio::io::{stdin, AsyncBufReadExt, BufReader};
 
 use serde::Serialize;
 use serde_json::json;
 
-use crate::base::{
-    bridge::{BridgeTx, Payload},
-    clock, StdoutConfig,
-};
+use crate::base::bridge::{BridgeTx, Payload};
+use crate::base::{clock, StdoutConfig};
+
+lazy_static::lazy_static! {
+    pub static ref COLORS_RE: Regex = Regex::new(r#"\u{1b}\[[0-9;]*m"#).unwrap();
+}
 
 #[derive(Debug, Serialize)]
 struct LogEntry {
     pub line: String,
+    pub tag: String,
     pub level: String,
-    #[serde(skip)]
-    pub timestamp: u64,
+    pub log_timestamp: String,
     pub message: String,
 }
 
 impl LogEntry {
-    fn parse(line: String) -> Self {
-        Self { line, level: "".to_string(), timestamp: clock() as u64, message: "".to_string() }
+    // NOTE: expects log lines to contain information in the format "{log_timestamp} {level} {tag}: {message}"
+    fn parse(line: String) -> Option<Self> {
+        // NOTE: remove any tty color escape characters
+        let line = COLORS_RE.replace_all(&line, "").trim().to_string();
+        let mut tokens = line.split(" ");
+        let log_timestamp = tokens.next()?.to_string();
+        let level = tokens.next()?.to_string();
+        let tag = tokens.next()?.trim_end_matches(':').to_string();
+        let message = tokens.fold("".to_string(), |acc, s| acc + " " + s).trim().to_string();
+
+        Some(Self { line, level, tag, log_timestamp, message })
     }
 }
 
@@ -51,15 +63,16 @@ impl Stdout {
             match lines.next_line().await? {
                 Some(l) => {
                     sequence += 1;
-                    let log_entry = LogEntry::parse(l);
-                    let payload = Payload {
-                        stream: self.config.stream_name.to_owned(),
-                        device_id: None,
-                        sequence,
-                        timestamp: log_entry.timestamp,
-                        payload: json!(log_entry),
-                    };
-                    self.tx.send_payload(payload).await;
+                    if let Some(log_entry) = LogEntry::parse(l) {
+                        let payload = Payload {
+                            stream: self.config.stream_name.to_owned(),
+                            device_id: None,
+                            sequence,
+                            timestamp: clock() as u64,
+                            payload: json!(log_entry),
+                        };
+                        self.tx.send_payload(payload).await;
+                    }
                 }
                 None => return Ok(()),
             }
