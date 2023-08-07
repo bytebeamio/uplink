4a9fb6562c3e6aecb6ff493cfd2eed1853469177
amokfa <iaansagar@gmail.com>
2023-08-07 16:19:52 +0530

    create new directories with correct permissions

--- uplink/src/collector/downloader.rs
+++ uplink/src/collector/downloader.rs
@@ -56,10 +56,11 @@ use serde::{Deserialize, Serialize};
 use tokio::time::timeout;
 
 use std::collections::HashMap;
-use std::fs::{create_dir_all, metadata, remove_dir_all, File};
+use std::fs::{metadata, remove_dir_all, File, Permissions, create_dir, set_permissions};
 use std::sync::Arc;
 use std::time::Duration;
 use std::{io::Write, path::PathBuf};
+use std::path::Path;
 
 use crate::base::bridge::BridgeTx;
 use crate::base::DownloaderConfig;
@@ -225,12 +226,31 @@ impl FileDownloader {
         Ok(())
     }
 
+    #[cfg(unix)]
+    fn create_dirs_with_perms(&self, path: &Path, perms: Permissions) -> std::io::Result<()> {
+        let mut current_path = PathBuf::new();
+
+        for component in path.components() {
+            current_path.push(component);
+
+            if !current_path.exists() {
+                create_dir(&current_path)?;
+                set_permissions(&current_path, perms.clone())?;
+            }
+
+        }
+
+        Ok(())
+    }
+
     /// Creates file to download into
     fn create_file(&self, name: &str, file_name: &str) -> Result<(File, String), Error> {
         // Ensure that directory for downloading file into, exists
         let mut download_path = PathBuf::from(self.config.path.clone());
         download_path.push(name);
-        create_dir_all(&download_path)?;
+        // do manual create_dir_all while setting permissions on each created directory
+        #[cfg(unix)]
+        self.create_dirs_with_perms(download_path.as_path(), std::os::unix::fs::PermissionsExt::from_mode(0o777))?;
 
         let mut file_path = download_path.to_owned();
         file_path.push(file_name);
