05e35366f776e91cd11e1acd5301fc33258ee4b9
Devdutt Shenoi <devdutt@bytebeam.io>
2023-08-12 15:26:52 +0530

    Revert unnecessary changes in "doc: add clarification requiring newline encoding"
    This reverts commit 5fb5726eda8f3792292789fba8c29aa69b614bca.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hello, World"
-
-TIME=
-echo "{ \"action_id\": \"\", \"sequence\": \"0\", \"timestamp\": \"$(date +%s%N | cut -b1-13)\", \"state\": \"Completed\", \"progress\": 100, \"errors\": [] }"
