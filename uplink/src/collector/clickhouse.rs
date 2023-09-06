8b8dea446041a1122fc220c904db9b358786c6e9
Devdutt Shenoi <devdutt@bytebeam.io>
2023-09-06 10:23:28 +0530

    fix: rowbinary format deserialization

--- uplink/src/collector/clickhouse.rs
+++ uplink/src/collector/clickhouse.rs
@@ -13,9 +13,9 @@ use crate::base::{
 
 #[derive(Debug, clickhouse::Row, Serialize, Deserialize)]
 struct QueryLog {
-    event_time: u64,
+    event_time: u32,
     query_duration_ms: u64,
-    read_rows: u32,
+    read_rows: u64,
     written_rows: u64,
     result_rows: u64,
     memory_usage: u64,
@@ -87,7 +87,7 @@ impl QueryLogReader {
         let where_clause = &config.where_clause;
         let sync_interval = config.interval;
         let query = format!(
-            "SELECT event_time, query_duration_ms, read_rows, written_rows, result_rows, memory_usage, current_database, query_id, query, query_kind, user, exception_code, exception FROM system.query_log WHERE {where_clause} AND event_time > (now() - toIntervalSecond({sync_interval}))"
+            "SELECT ?fields FROM system.query_log WHERE {where_clause} AND event_time > (now() - toIntervalSecond({sync_interval}))"
         );
         info!("Query: {query}");
         Self { config, client, query, bridge_tx, sequence: 0 }
